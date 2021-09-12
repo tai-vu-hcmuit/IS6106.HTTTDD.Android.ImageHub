9ca513414eeb00149e745f2f0514755199ca903a
vcttai <vcttai@gmail.com>
2021-09-12 22:22:01 +0700

    Section 6: PagingSource & PagingData

--- app/src/main/java/taivu/uit/htttdd/imagehub/data/UnsplashRepository.kt
+++ app/src/main/java/taivu/uit/htttdd/imagehub/data/UnsplashRepository.kt
@@ -2,6 +2,19 @@ package taivu.uit.htttdd.imagehub.data
 
 import taivu.uit.htttdd.imagehub.api.UnsplashApi
 import javax.inject.Inject
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
+
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query) }
+        ).liveData
 }
--- app/src/main/java/taivu/uit/htttdd/imagehub/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/taivu/uit/htttdd/imagehub/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,22 @@
 package taivu.uit.htttdd.imagehub.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import taivu.uit.htttdd.imagehub.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragement_gallery) {
+
+    private val viewModel by viewModels<GalleryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+
+        }
+    }
 }
