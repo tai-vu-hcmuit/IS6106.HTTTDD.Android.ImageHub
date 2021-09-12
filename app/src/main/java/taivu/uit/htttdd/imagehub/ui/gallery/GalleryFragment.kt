c103cef8b558ecf7ae2e337b6913d16b367a8fa5
vcttai <vcttai@gmail.com>
2021-09-12 22:40:53 +0700

    Section 7: PagingDataAdapter

--- app/src/main/java/taivu/uit/htttdd/imagehub/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/taivu/uit/htttdd/imagehub/ui/gallery/GalleryFragment.kt
@@ -5,18 +5,36 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import taivu.uit.htttdd.imagehub.R
+import taivu.uit.htttdd.imagehub.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GalleryFragment : Fragment(R.layout.fragement_gallery) {
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel>()
+
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        _binding = FragmentGalleryBinding.bind(view)
+
+        val adapter = UnsplashPhotoAdapter()
+
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
+        }
+
         viewModel.photos.observe(viewLifecycleOwner) {
 
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
