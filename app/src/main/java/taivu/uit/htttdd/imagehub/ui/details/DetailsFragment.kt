fbe1974d6613d23917d79a47810206f4911c9b14
vcttai <vcttai@gmail.com>
2021-09-14 22:55:51 +0700

    Section 11: Navigate to the Detail screen

---
+++ app/src/main/java/taivu/uit/htttdd/imagehub/ui/details/DetailsFragment.kt
@@ -0,0 +1,7 @@
+package taivu.uit.htttdd.imagehub.ui.details
+
+import androidx.fragment.app.Fragment
+import taivu.uit.htttdd.imagehub.R
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+}
--- app/src/main/java/taivu/uit/htttdd/imagehub/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/taivu/uit/htttdd/imagehub/ui/gallery/GalleryFragment.kt
@@ -8,13 +8,15 @@ import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
 import taivu.uit.htttdd.imagehub.R
 import taivu.uit.htttdd.imagehub.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
+import taivu.uit.htttdd.imagehub.data.UnsplashPhoto
 
 @AndroidEntryPoint
-class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+class GalleryFragment : Fragment(R.layout.fragment_gallery), UnsplashPhotoAdapter.OnItemClickListener {
 
     private val viewModel by viewModels<GalleryViewModel>()
 
@@ -26,7 +28,7 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
         _binding = FragmentGalleryBinding.bind(view)
 
-        val adapter = UnsplashPhotoAdapter()
+        val adapter = UnsplashPhotoAdapter(this)
 
         binding.apply {
             recyclerView.setHasFixedSize(true)
@@ -95,5 +97,10 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
                 return true
             }
         })
+    }
+
+    override fun onItemClick(photo: UnsplashPhoto) {
+        val action = GalleryFragmentDirections.actionGalleryFragmentToDetailsFragment(photo)
+        findNavController().navigate(action)
     }
 }
