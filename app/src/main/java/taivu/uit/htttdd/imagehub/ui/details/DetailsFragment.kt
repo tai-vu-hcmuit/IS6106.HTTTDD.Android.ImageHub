898d6eea5cfc8ef3a3160b535f1d5bc35a581059
vcttai <vcttai@gmail.com>
2021-09-25 14:34:37 +0700

    Section 14: Save image

--- app/src/main/java/taivu/uit/htttdd/imagehub/ui/details/DetailsFragment.kt
+++ app/src/main/java/taivu/uit/htttdd/imagehub/ui/details/DetailsFragment.kt
@@ -1,20 +1,33 @@
 package taivu.uit.htttdd.imagehub.ui.details
 
+import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import taivu.uit.htttdd.imagehub.R
 import taivu.uit.htttdd.imagehub.databinding.FragmentDetailsBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
     private val args by navArgs<DetailsFragmentArgs>()
@@ -51,9 +64,11 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
                         progressBar.isVisible = false
                         textViewCreator.isVisible = true
                         textViewDescription.isVisible = photo.description != null
+                        btSaveImg.isVisible = true
                         return false
                     }
                 })
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(imageView)
 
             textViewDescription.text = photo.description
@@ -68,6 +83,47 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
                 }
                 paint.isUnderlineText = true
             }
+
+            btSaveImg.setOnClickListener{
+                Log.e("a", photo.urls.full.toString());
+
+                // Get image bitmap from imageView
+                val bitmapDrawable = imageView.drawable as BitmapDrawable
+                val bitmap = bitmapDrawable.bitmap
+
+                val filename = "Imagehub_${System.currentTimeMillis()}.jpg"
+                var fos: OutputStream? = null
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    context?.contentResolver?.also { resolver ->
+
+                        //Content resolver will process the contentvalues
+                        val contentValues = ContentValues().apply {
+                            put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                        }
+
+                        //Inserting the contentValues to contentResolver and getting the Uri
+                        val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                        //Opening an outputstream with the Uri that we got
+                        fos = imageUri?.let { resolver.openOutputStream(it) }
+                    }
+                } else {
+                    val imagesDir =
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                    val image = File(imagesDir, filename)
+                    fos = FileOutputStream(image)
+                }
+
+                fos?.use {
+                    //Finally writing the bitmap to the output stream that we opened
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+                    Toast.makeText(imageView.context, getString(R.string.save_success), Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
+
 }
--- app/src/main/java/taivu/uit/htttdd/imagehub/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/taivu/uit/htttdd/imagehub/ui/gallery/GalleryViewModel.kt
@@ -27,7 +27,7 @@ class GalleryViewModel @Inject constructor(
 
     companion object {
         private const val CURRENT_QUERY = "current_query"
-        private const val DEFAULT_QUERY = "cats"
+        private const val DEFAULT_QUERY = "statue of liberty"
     }
 
 }
